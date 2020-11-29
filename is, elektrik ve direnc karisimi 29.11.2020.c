6b0dd3932c23edafcfe7613523df8965c0f0a16c
3HHH3 <75170637+3HHH3@users.noreply.github.com>
2020-11-29 21:33:14 +0300

    Update is, elektrik ve direnc karisimi 29.11.2020.c

--- is, elektrik ve direnc karisimi 29.11.2020.c
+++ is, elektrik ve direnc karisimi 29.11.2020.c
@@ -44,7 +44,6 @@ int main()
    scanf("%d", &secim1);
    printf("\n\n");
 
-
    switch (secim1) //İs ve Enerji, Elektrik gucu arasından secim yapıyoruz
    {
    case 1: //*****İs ve Enerji*****
@@ -192,94 +191,72 @@ int main()
    case 3: //*****Direncin gucu formülü*****
    {
 
-   printf("Direncin gücüne hoşldiniz \n\n");
-   printf("Lütfen yapmak istediğiniz işlemi seçiniz \n");
-   printf("p=w/t = 1,\n");
-   printf("p=v*i = 2,\n");
-   printf("p=i^2*r = 3,\n");
-   printf("p=v^2/r = 4,\n");
-   printf("tuşlayınız: ");
-   scanf("%d", &secim4);
+      printf("Direncin gücüne hoşldiniz \n\n");
+      printf("Lütfen yapmak istediğiniz işlemi seçiniz \n");
+      printf("p=w/t = 1,\n");
+      printf("p=v*i = 2,\n");
+      printf("p=i^2*r = 3,\n");
+      printf("p=v^2/r = 4,\n");
+      printf("tuşlayınız: ");
+      scanf("%d", &secim4);
 
-   switch (secim4)//p=w/t, p=v*i, p=i^2*r, p=v^2/r arasından seciyoruz
-   {
-   case 1: //p=w/t
+      switch (secim4) //p=w/t, p=v*i, p=i^2*r, p=v^2/r arasından seciyoruz
+      {
+      case 1: //p=w/t
 
-   {
-      printf("\n\np=w/t işlemini seçtiniz.\n\n");
-      printf("Lütfen (w) değerini yazınız: ");
-      scanf("%f", &w);
-      printf("Lütfen (t) değerini yazınız: ");
-      scanf("%f", &t);
-      p = w / t;
-      printf("p Sonucu: %.3f", p);
-      break;
-   }
+      {
+         printf("\n\np=w/t işlemini seçtiniz.\n\n");
+         printf("Lütfen (w) değerini yazınız: ");
+         scanf("%f", &w);
+         printf("Lütfen (t) değerini yazınız: ");
+         scanf("%f", &t);
+         p = w / t;
+         printf("p Sonucu: %.3f", p);
+         break;
+      }
 
-   case 2: //p=v*i
+      case 2: //p=v*i
 
-   {
-      printf("\n\np=v*i işlemini seçtiniz.\n\n");
-      printf("Lütfen (v) değerini yazınız: ");
-      scanf("%f", &v);
-      printf("Lütfen (i) değerini yazınız: ");
-      scanf("%f", &i);
-      p = v * i;
-      printf("p Sonucu: %.3f", p);
-      break;
-   }
-   case 3: //p=i^2*r
+      {
+         printf("\n\np=v*i işlemini seçtiniz.\n\n");
+         printf("Lütfen (v) değerini yazınız: ");
+         scanf("%f", &v);
+         printf("Lütfen (i) değerini yazınız: ");
+         scanf("%f", &i);
+         p = v * i;
+         printf("p Sonucu: %.3f", p);
+         break;
+      }
+      case 3: //p=i^2*r
 
-   {
-      printf("\n\np=i^2*r işlemini seçtiniz.\n\n");
-      printf("Lütfen (i) değerini yazınız: ");
-      scanf("%f", &i);
-      printf("Lütfen (r) değerini yazınız: ");
-      scanf("%f", &r);
-      p = i * i * r;
-      printf("p Sonucu: %.3f", p);
-      break;
-   }
+      {
+         printf("\n\np=i^2*r işlemini seçtiniz.\n\n");
+         printf("Lütfen (i) değerini yazınız: ");
+         scanf("%f", &i);
+         printf("Lütfen (r) değerini yazınız: ");
+         scanf("%f", &r);
+         p = i * i * r;
+         printf("p Sonucu: %.3f", p);
+         break;
+      }
 
-   case 4: //p=v^2/r
+      case 4: //p=v^2/r
 
-   {
-      printf("\n\np=v^2/r işlemini seçtiniz.\n\n");
-      printf("Lütfen (v) değerini yazınız: ");
-      scanf("%f", &v);
-      printf("Lütfen (r) değerini yazınız: ");
-      scanf("%f", &r);
-      p = (v * v) / r;
-      printf("p Sonucu: %.3f", p);
-      break;
-   }
-   default://hata oldugunda cıkacak eror4
-   {
-      printf("\n\nHATA: Bilinmeyen bir değer girdiniz! EROR 4");
-   }
-   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      {
+         printf("\n\np=v^2/r işlemini seçtiniz.\n\n");
+         printf("Lütfen (v) değerini yazınız: ");
+         scanf("%f", &v);
+         printf("Lütfen (r) değerini yazınız: ");
+         scanf("%f", &r);
+         p = (v * v) / r;
+         printf("p Sonucu: %.3f", p);
+         break;
+      }
+      default: //hata oldugunda cıkacak eror4
+      {
+         printf("\n\nHATA: Bilinmeyen bir değer girdiniz! EROR 4");
+      }
+      }
 
       break;
    }
