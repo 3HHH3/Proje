bfe429d1c9a4e5f973f7ee6732dc89f25b9f280c
3HHH3 <75170637+3HHH3@users.noreply.github.com>
2020-12-16 20:01:33 +0300

    Update soru3.c

--- olası sorular/soru3.c
+++ olası sorular/soru3.c
@@ -1,37 +1,39 @@
 /*
-İki tam sayı alacak ve verilecek operatöre 
-göre (+, -, *, /, %) işlem yapacak bir program yazınız.
+Hasan Hüseyin Harmancı
+İki tam sayı alacak ve verilecek operatöre göre 
+(+, -, *, /, %) işlem yapacak bir program yazınız.
 */
+
 #include <stdio.h>
-int main(void)
+int main()
 {
-   int sayi_1, sayi_2;
-   char operator_simgesi;
-   printf("Lütfen işlem simgesi giriniz> ");
-   scanf("%c", &operator_simgesi);
-   printf("Lütfen birinci sayıyı giriniz> ");
-   scanf("%d", &sayi_1);
-   printf("Lütfen ikinci sayıyı giriniz> ");
-   scanf("%d", &sayi_2);
-   switch (operator_simgesi)
-   {
-   case '+':
-      printf("%d %c %d = %d\n", sayi_1, operator_simgesi, sayi_2, sayi_1 + sayi_2);
-      break;
-   case '-':
-      printf("%d %c %d = %d\n", sayi_1, operator_simgesi, sayi_2, sayi_1 - sayi_2);
-      break;
-   case '%':
-      printf("%d %c %d = %d\n", sayi_1, operator_simgesi, sayi_2, sayi_1 % sayi_2);
-      break;
-   case '*':
-      printf("%d %c %d = %d\n", sayi_1, operator_simgesi, sayi_2, sayi_1 * sayi_2);
-      break;
-   case '/':
-      printf("%d %c %d = %.2f\n", sayi_1, operator_simgesi, sayi_2, (float)sayi_1 / sayi_2);
-      break;
-   default:
-      printf("HATA: Tanımsız bir operatör girdiniz!\n");
-   }
-   return 0;
+    int sayi1, sayi2; // int veri tipinde 2 değişken olusturduk
+    char secim;       //char veri tipinde değişken olusturduk
+    printf("Lütfen işlem simgesi giriniz> ");
+    scanf("%c", &secim); //klavyeden alıgımız degeri secım adlı degişkene attık
+    printf("Lütfen birinci sayıyı giriniz> ");
+    scanf("%d", &sayi1);                      //klavyeden alıgımız degeri sayi1 adlı degişkene attık
+    printf("Lütfen ikinci sayıyı giriniz> "); //klavyeden alıgımız degeri sayi2 adlı degişkene attık
+    scanf("%d", &sayi2);
+    switch (secim)
+    {         // klavyeden yazdıgımız degeri switch ile sorguluyoruz
+    case '+': // secım degeri + oldugunda asagıdaki kod çalısacaktır
+        printf("%d %c %d = %d\n", sayi1, secim, sayi2, sayi1 + sayi2);
+        break;
+    case '-': // secım degeri - oldugunda asagıdaki kod çalısacaktır
+        printf("%d %c %d = %d\n", sayi1, secim, sayi2, sayi1 - sayi2);
+        break;
+    case '%': // secım degeri % oldugunda asagıdaki kod çalısacaktır
+        printf("%d %c %d = %d\n", sayi1, secim, sayi2, sayi1 % sayi2);
+        break;
+    case '*': // secım degeri * oldugunda asagıdaki kod çalısacaktır
+        printf("%d %c %d = %d\n", sayi1, secim, sayi2, sayi1 * sayi2);
+        break;
+    case '/': // secım degeri / oldugunda asagıdaki kod çalısacaktır
+        printf("%d %c %d = %.2f\n", sayi1, secim, sayi2, (float)sayi1 / sayi2);
+        break;
+    default: // yukarıdakı degerlerden baska deger secıldıgınde asagıdakı kod calısacaktır
+        printf("HATA: Tanımsız bir operatör girdiniz!\n");
+    }
+    return 0;
 }
